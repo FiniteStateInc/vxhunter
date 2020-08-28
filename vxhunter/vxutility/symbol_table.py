1def6166678c8b8f6a1c78fd5bc4be1e1f5175bc
Tim dW <tim@finitestate.io>
2020-08-28 00:00:23 -0400

    Changes to dynamically detect the base address of the binary and retry the symtab resolution.

--- vxhunter/vxutility/symbol_table.py
+++ vxhunter/vxutility/symbol_table.py
@@ -1,7 +1,10 @@
+import re
+import struct
 import struct as st
+import sys
 
-from common import endy_str, word_str, word_size, read_data_at, maybe_get_string_at, is_offset_in_current_program, SUPPORTED_VX_VERSIONS
-
+from common import endy_str, word_str, word_size, read_data_at, maybe_get_string_at, is_offset_in_current_program, \
+    find_bytes_address, print_out, set_base_address, get_is_big_endian
 
 # Symbol types for VxWorks 5.5
 VX5_SYM_TYPES = [
@@ -39,22 +42,14 @@ VX6_SYM_TYPES = [
 SYM_TYPES = {
     5: VX5_SYM_TYPES,
     6: VX6_SYM_TYPES,
-    7: VX6_SYM_TYPES # same as 6
+    7: VX6_SYM_TYPES  # same as 6
 }
 
 # Symbol types that live in the .text section
-TEXT_SYM_TYPES = {
-    5: [4, 5],
-    6: [4, 5],
-    7: [4, 5]
-}
+TEXT_SYM_TYPES = {5: [4, 5], 6: [4, 5], 7: [4, 5]}
 
 # Symbol types that live in the .data section
-DATA_SYM_TYPES = {
-    5: [6, 7],
-    6: [8, 9],
-    7: [8, 9]
-}
+DATA_SYM_TYPES = {5: [6, 7], 6: [8, 9], 7: [8, 9]}
 
 # The amount of entries in a candidate symbol table needed for it to be chosen as the actual symbol table
 SYMTAB_MIN_COUNT = 100
@@ -65,11 +60,12 @@ def get_symbol_fmt(endy_str, word_size_str, vx_ver):
     Return the `struct` format for the symbol struct depending on the VxWorks version and the size of said struct.
     Note: This function assumes that `vx_ver` has already been validated.
     '''
-    if vx_ver == 5: fmt = endy_str + (word_size_str * 3) + 'H' + 'BB'
-    else:           fmt = endy_str + (word_size_str * 4) + 'H' + 'BB'
+    if vx_ver == 5:
+        fmt = endy_str + (word_size_str * 3) + 'H' + 'BB'
+    else:
+        fmt = endy_str + (word_size_str * 4) + 'H' + 'BB'
 
     return fmt, st.calcsize(fmt)
-
 
 
 def sym_dict(offset, name_ptr, val_ptr, grp, sym_type, null):
@@ -81,15 +77,13 @@ def sym_dict(offset, name_ptr, val_ptr, grp, sym_type, null):
     }
 
 
-
 def is_sym_valid(sym_types, name_ptr, val_ptr, grp, sym_type, null):
     is_sym = True
-    is_sym &= sym_type in sym_types # expect that the symbol type is in the list of symbol types for the vx version
-    is_sym &= null == 0             # expect that the null struct-terminator is indeed null
-    is_sym &= grp == 0              # expect that the symbol group is null
-    is_sym &= name_ptr != 0         # expect that the symbol name is not null
+    is_sym &= sym_type in sym_types  # expect that the symbol type is in the list of symbol types for the vx version
+    is_sym &= null == 0  # expect that the null struct-terminator is indeed null
+    is_sym &= grp == 0  # expect that the symbol group is null
+    is_sym &= name_ptr != 0  # expect that the symbol name is not null
     return is_sym
-
 
 
 def parse_sym(offset, sym_st_fmt, sym_size, sym_types):
@@ -110,6 +104,39 @@ def parse_sym(offset, sym_st_fmt, sym_size, sym_types):
     return sym_dict(offset, *fields), is_sym_valid(sym_types, *fields)
 
 
+def fix_image_base(blk, vx_ver):
+    """Note: this may seem redundant to duplicate effort, trying to find symtable here and then again
+    in get_symtab_bounds(). However rebasing is needed
+    """
+    sym_st_fmt, sym_st_size = get_symbol_fmt(endy_str, word_str, vx_ver)
+    sym_types = SYM_TYPES[vx_ver]
+
+    left_off, symtab = find_sym_table(blk, blk.start.offset, sym_st_fmt, sym_st_size, sym_types, vx_ver)
+    if len(symtab) < SYMTAB_MIN_COUNT:
+        # If no matches were found it could be that they were thrown out because the offset is so large
+        # that none of the syms match fall in the memory range of the image.
+        # Try again, this time without verifying the addresses.
+        left_off, symtab = find_sym_table(blk, blk.start.offset, sym_st_fmt, sym_st_size, sym_types, vx_ver,
+                                          verify_addresses=False)
+
+        # throw out the candidate symbol table and return if it's below the threshold count
+        if len(symtab) < SYMTAB_MIN_COUNT:
+            return None
+
+    #for sym in symtab:
+    #    print_out("name 0x%08x" %sym['name_addr'])
+
+    symtab = sorted(symtab, key=lambda x: x['offset'])
+
+    # if we have a block at offset zero, it's probably not correct...
+    if 0 == blk.start.offset:
+        new_base = try_rebase(blk, symtab, vx_ver)
+        if new_base:
+            return new_base
+
+    return None
+
+
 def get_symtab_bounds(blk, vx_ver, symbol_fn):
     '''
     Try to get the VxWorks 5, 6, or 7 symbol table.
@@ -117,49 +144,7 @@ def get_symtab_bounds(blk, vx_ver, symbol_fn):
     sym_st_fmt, sym_st_size = get_symbol_fmt(endy_str, word_str, vx_ver)
     sym_types = SYM_TYPES[vx_ver]
 
-    symtab = []
-    i = blk.start.offset
-
-    while i < blk.end.offset - sym_st_size:
-        sym, sym_valid = parse_sym(i, sym_st_fmt, sym_st_size, sym_types)
-
-        # if the current symbol struct is not a valid symbol, only increment the cursor by `word_size` bytes
-        # in case we are unaligned with the symbol table (this assumes the symbol table is word-aligned)
-        if sym is None or not sym_valid:
-            symtab = []
-            i += word_size
-            continue
-
-        name_addr = sym['name_addr']
-
-        # the name pointer must be within the bounds of the memory block
-        #if name_addr < blk.start.offset or name_addr >= blk.end.offset:
-        if not is_offset_in_current_program(name_addr):
-            symtab = []
-            i += word_size
-            continue
-
-        sym_type = sym['flag']
-
-        # if the symbol type resides in the .text or .data section, make sure it's dest pointer is within the block
-        if sym_type in TEXT_SYM_TYPES[vx_ver] or sym_type in DATA_SYM_TYPES[vx_ver]:
-            dest_addr = sym['dest_addr']
-
-            #if dest_addr < blk.start.offset or dest_addr >= blk.end.offset:
-            if not is_offset_in_current_program(dest_addr):
-                symtab = []
-                i += word_size
-                continue
-
-        symtab.append(sym)
-
-        # increase the cursor by the symbol size since we have a valid symbol
-        i += sym_st_size
-
-        # if we have 100 consecutive symbols, assume that this is indeed the symbol table
-        if len(symtab) >= SYMTAB_MIN_COUNT:
-            break
-
+    left_off, symtab = find_sym_table(blk, blk.start.offset, sym_st_fmt, sym_st_size, sym_types, vx_ver)
 
     # throw out the candidate symbol table and return if it's below the threshold count
     if len(symtab) < SYMTAB_MIN_COUNT:
@@ -168,6 +153,8 @@ def get_symtab_bounds(blk, vx_ver, symbol_fn):
     symtab = sorted(symtab, key=lambda x: x['offset'])
     symtab_start = symtab[0]['offset']
     symtab_end = symtab[-1]['offset']
+
+    print_out('Initial Symbol table found from %08x to %08x' % (symtab_start, symtab_end))
 
     # add the current symbols to the program
     for sym in symtab:
@@ -178,7 +165,7 @@ def get_symtab_bounds(blk, vx_ver, symbol_fn):
             symbol_fn(sym)
 
     # get the rest of the symbol table and them to the program
-    for i in range(i, blk.end.offset - sym_st_size, sym_st_size):
+    for i in range(left_off, blk.end.offset - sym_st_size, sym_st_size):
         sym, sym_valid = parse_sym(i, sym_st_fmt, sym_st_size, sym_types)
 
         # once we have seen an invalid symbol or failed at reading, break out of the loop
@@ -197,3 +184,126 @@ def get_symtab_bounds(blk, vx_ver, symbol_fn):
     return symtab_start, symtab_end
 
 
+def find_sym_table(blk, start_addr, sym_st_fmt, sym_st_size, sym_types, vx_ver, verify_addresses=True):
+    i = start_addr
+    symtab = []
+    while i < blk.end.offset - sym_st_size:
+
+        sym, sym_valid = parse_sym(i, sym_st_fmt, sym_st_size, sym_types)
+
+        # if the current symbol struct is not a valid symbol, only increment the cursor by `word_size` bytes
+        # in case we are unaligned with the symbol table (this assumes the symbol table is word-aligned)
+        if sym is None or not sym_valid:
+            symtab = []
+            i += word_size
+            continue
+
+        name_addr = sym['name_addr']
+
+        # the name pointer must be within the bounds of the memory block
+        # if name_addr < blk.start.offset or name_addr >= blk.end.offset:
+        if verify_addresses:
+            if not is_offset_in_current_program(name_addr):
+                symtab = []
+                i += word_size
+                continue
+
+        sym_type = sym['flag']
+
+        # if the symbol type resides in the .text or .data section, make sure it's dest pointer is within the block
+        if sym_type in TEXT_SYM_TYPES[vx_ver] or sym_type in DATA_SYM_TYPES[vx_ver]:
+            dest_addr = sym['dest_addr']
+
+            # if dest_addr < blk.start.offset or dest_addr >= blk.end.offset:
+            if verify_addresses:
+                if not is_offset_in_current_program(dest_addr):
+                    symtab = []
+                    i += word_size
+                    continue
+
+        sys.stdout.write('.')
+        symtab.append(sym)
+
+        # increase the cursor by the symbol size since we have a valid symbol
+        i += sym_st_size
+
+        # if we have 100 consecutive symbols, assume that this is indeed the symbol table
+        if len(symtab) >= SYMTAB_MIN_COUNT:
+            break
+
+    return i, symtab
+
+
+def try_rebase(blk, sym_table, vx_ver):
+    '''Detected that base is probably not right. This will cause symbols to not be generated correctly
+    So try to find the strings table with function symbol names
+    Find the highest pointer to a function name,
+    Subtract the two, AND by 0xFFFF0000 to get an offset that we can apply
+    '''
+    sym_address = None
+
+    # 'VxWorks' occurs just before the table of function name strings. this doesn't need to be perfect
+    # just need something close to the start of the table will do.
+    # Probably need to make this more robust, need to test with a larger set of images.
+    if vx_ver > 5:
+        search_str = '\x00VxWorks\x00'
+    else:
+        search_str = '\x00_GLOBAL_\x00'
+
+    search_result = find_bytes_address(blk.start.offset, search_str)
+    if search_result:
+        sym_address = int(search_result.toString(), 16)
+        # since we're searching with a leading null, we need to increment address by 1.
+        sym_address = sym_address + 1
+
+        max_ptr = 0
+        for sym in sym_table:
+            if sym['name_addr'] > max_ptr:
+                max_ptr = sym['name_addr']
+
+        print_out(
+            'Base doesn\'t look correct... trying to verify base is correct (name_addr)%08x - (Key_string)%08x' % (
+                max_ptr, sym_address))
+        initial_offset = (max_ptr - sym_address) & 0xFFF00000
+        print_out('initial_offset: 0x%08x' % initial_offset)
+
+        offset = fuzzy_search_for_function_names(initial_offset, sym_table)
+
+        if offset:
+            print_out('Believe the base address is incorrect... trying to fix it... Fingers crossed')
+            set_base_address(offset)
+            return offset
+
+    return None
+
+
+def is_valid_function_name(name):
+    return re.search(r'^[A-Za-z_0-9-]+$', name)
+
+
+def fuzzy_search_for_function_names(offset, sym_table):
+    """There doesn't seem to be a specific MASK that we can apply that works for all images, so lets try incrementing
+    with 0x1000 steps, and determine if there is an offset where all the names resolve correctly.
+    """
+    for i in range(0, 0xF0000, 0x1000):
+    #for i in range(0, 0x10000, 0x1000):
+        invalid_function_found = False
+        curr_offset = offset + i
+        print('Testing using offset 0x%08x' % curr_offset)
+        for sym in sym_table:
+            name = maybe_get_string_at(sym['name_addr'] - curr_offset)
+            if name:
+                if not is_valid_function_name(name):
+                    invalid_function_found = True
+                    print("name invalid %s" % name)
+                    break
+                else:
+                    print("VALID: %s" % name)
+            else:
+                print("invalid (%s) name at %08x" %( name, sym['name_addr'] - curr_offset))
+                invalid_function_found = True
+                break
+
+        if not invalid_function_found:
+            # If all symbols resolved with sane function names, then we have a match.
+            return curr_offset
--- vxhunter/vxworks_symtab.py
+++ vxhunter/vxworks_symtab.py
@@ -1,9 +1,10 @@
 import sys
+
 sys.path.insert(0, '.')
 
-from vxutility.common import get_main_memory, auto_analyze, get_memory_blocks, print_err, print_out, get_args
+from vxutility.common import get_memory_blocks, print_err, print_out, get_args, get_image_base, maybe_get_string_at
 from vxutility.symbol import add_symbol, get_symbol, create_symbol_table
-from vxutility.symbol_table import get_symtab_bounds
+from vxutility.symbol_table import get_symtab_bounds, fix_image_base
 
 SYMBOL_KEYS = ['name', 'name_addr', 'dest_addr', 'flag']
 
@@ -23,27 +24,24 @@ def define_symbol_table(vx_ver):
         print_out('Symbol table already defined', script_name)
         return True
 
+    # It's possible that our firmware has been loaded with multiple memory blocks, maybe by a previous add-on.
+    # The symbol table could be in any of them so we hafta search them all
     # Try to get the symbol table bounds
     symtab_bounds = None
-
-    # It's possible that our firmware has been loaded with multiple memory blocks, maybe by a previous add-on.
-    # The symbol table could be in any of them so we hafta search them all.
     for blk in get_memory_blocks():
-        symtab_bounds = get_symtab_bounds(blk,
-                                          vx_ver,
-                                          add_symbol_wrapper)
+        symtab_bounds = get_symtab_bounds(blk, vx_ver, add_symbol_wrapper)
 
         if symtab_bounds is not None:
             break
 
-
     if symtab_bounds is None:
-        print_err('Could not find symbol table bounds', script_name)
-        analyzeAll(currentProgram)
-        return False
+        #print_err('Could not find symbol table bounds', script_name)
+        raise RuntimeError('Could not find symbol table bounds', script_name)
+        # analyzeAll(currentProgram)
+        #return False
 
-    print_out('Symbol table found from %08x to %08x' % symtab_bounds, script_name)
     symtab_start, symtab_end = symtab_bounds
+    print_out('Symbol table found from %08x to %08x' % (symtab_start, symtab_end))
 
     # Create the symbol table struct.
     create_symbol_table(symtab_start, symtab_end, vx_ver)
@@ -57,5 +55,18 @@ if __name__ == '__main__':
         exit()
 
     # Try to get the symbol table/define the symbols.
-    define_symbol_table(vx_ver)
+    try:
+       define_symbol_table(vx_ver)
+    except (ghidra.util.exception.InvalidInputException, RuntimeError):
+        # stuff went wrong!!! we should try see if rebasing will work
+        if 0 == get_image_base().offset and 1 == len(get_memory_blocks()):
+            # 0 should not be a valid memory address, and we have a monolithic image
+            # so proceed with trying to determine the base
+            blk = get_memory_blocks()[0]
+            base = fix_image_base(blk, vx_ver)
+            if base:
+                print_out('Fixed Base: 0x%08x' % base)
 
+                define_symbol_table(vx_ver)
+            else:
+                print_err('Sorry couldn\'t rebase the binary')
